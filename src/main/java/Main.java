f986533a92014976d192556f5bf0bddca02021fb
Mannelig92 <mannelig92@gmail.com>
2021-09-24 02:04:56 +0300

    v1.0

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Ints ints = new IntsCalculator();
+
+        System.out.println(ints.sum(4, 5));
+        System.out.println(ints.mult(2, 6));
+        System.out.println(ints.pow(6, 3));
+    }
+}
